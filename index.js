e0919e85a1c6266935d00c8f4ab602866da07e0f
Jenni Puhakka <jenni.puhakka@aalto.fi>
2021-04-21 16:00:06 +0300

    uibuild

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ app.post('/api/persons', (request, response) => {
     })
   })
 
-app.put('/api/notes/:id', (request, response, next) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
   
     const person = {
